ad54695df43d4ea077f90a846d9f078face89395
Ngoc EnterVN <HNC@gmail.com>
2025-02-06 16:38:11 +0700

    Sua index.html

--- vietnamlens/src/routers.rs
+++ vietnamlens/src/routers.rs
@@ -3,3 +3,15 @@ use actix_web::{get, HttpResponse, Responder};
 pub async fn hello_world() -> impl Responder {
     HttpResponse::Ok().body("<h1>Xin chào! Chúng tôi là Vietnamlens!</h1>")
 }
+
+// struct Todo{
+//     task: String,
+// }
+// #[get("/todos/list")]
+// pub async fn todos_list() -> impl Responder {
+//     let todos: Vec<Todo> = vec![
+//         Todo{
+//             task: "Làm Web Vietnamlens".to_string(),
+//         },
+//     ];
+// } 
